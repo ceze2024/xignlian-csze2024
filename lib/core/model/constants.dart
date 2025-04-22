335c7c997a2d057e479253c3074e5334fc58086d
ceze2024 <csze2024@outlook.com>
2025-04-22 21:18:44 +0800

    Revert "构建"
    This reverts commit 8fbdcc2d76160ad51cc8117dcb53938cfa19c5bc.

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,12 +1,12 @@
 abstract class Constants {
   static const appName = "星链";
-  static const githubUrl = "https://www.starlinkvpn.cc";
-  static const githubReleasesApiUrl = "https://www.starlinkvpn.cc";
-  static const githubLatestReleaseUrl = "https://www.starlinkvpn.cc";
-  static const appCastUrl = "https://www.starlinkvpn.cc";
+  static const githubUrl = "https://www.startlinkvpn.cc";
+  static const githubReleasesApiUrl = "https://www.startlinkvpn.cc";
+  static const githubLatestReleaseUrl = "https://www.startlinkvpn.cc";
+  static const appCastUrl = "https://www.startlinkvpn.cc";
   static const telegramChannelUrl = "https://t.me/Starlink123456";
-  static const privacyPolicyUrl = "https://www.starlinkvpn.cc";
-  static const termsAndConditionsUrl = "https://www.starlinkvpn.cc";
+  static const privacyPolicyUrl = "https://www.startlinkvpn.cc";
+  static const termsAndConditionsUrl = "https://www.startlinkvpn.cc";
   static const cfWarpPrivacyPolicy = "https://www.cloudflare.com/application/privacypolicy/";
   static const cfWarpTermsOfService = "https://www.cloudflare.com/application/terms/";
 }
--- scripts/package_windows.ps1
+++ scripts/package_windows.ps1
@@ -4,14 +4,14 @@ New-Item -ItemType Directory -Force -Name "out"
 # windows setup
 # Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "dist\tmp\hiddify-next-setup.exe" -ErrorAction SilentlyContinue
 # Compress-Archive -Force -Path "dist\tmp\hiddify-next-setup.exe",".github\help\mac-windows\*.url" -DestinationPath "out\hiddify-windows-x64-setup.zip"
-Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "out\starlinkvpn-Windows-Setup-x64.exe" -ErrorAction SilentlyContinue
-Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows.msix" | Copy-Item -Destination "out\starlinkvpn-Windows-Setup-x64.msix" -ErrorAction SilentlyContinue
+Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "out\startlink-Windows-Setup-x64.exe" -ErrorAction SilentlyContinue
+Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows.msix" | Copy-Item -Destination "out\startlink-Windows-Setup-x64.msix" -ErrorAction SilentlyContinue
 
 
 # windows portable
-xcopy "build\windows\x64\runner\Release" "dist\tmp\starlinkvpn-next" /E/H/C/I/Y
-xcopy ".github\help\mac-windows\*.url" "dist\tmp\starlinkvpn-next" /E/H/C/I/Y
-Compress-Archive -Force -Path "dist\tmp\starlinkvpn-next" -DestinationPath "out\starlinkvpn-Windows-Portable-x64.zip" -ErrorAction SilentlyContinue
+xcopy "build\windows\x64\runner\Release" "dist\tmp\startlink-next" /E/H/C/I/Y
+xcopy ".github\help\mac-windows\*.url" "dist\tmp\startlink-next" /E/H/C/I/Y
+Compress-Archive -Force -Path "dist\tmp\startlink-next" -DestinationPath "out\startlink-Windows-Portable-x64.zip" -ErrorAction SilentlyContinue
 
 Remove-Item -Path "$HOME\.pub-cache\git\cache\flutter_circle_flags*" -Force -Recurse -ErrorAction SilentlyContinue
 
